dec3e1c9fad891fdb9cfd85d936428f313485874
thrombash <nickel2730@ya.ru>
2024-05-25 22:07:34 +0400

    first commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
